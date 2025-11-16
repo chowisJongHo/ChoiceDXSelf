9d3d70f2e8cb6d204b825de1b4e24719d042873e
JonghoKim <pia-a@hanmail.net>
2025-11-16 15:51:54 +0900

    SettingsFragment added

---
+++ app/src/main/java/com/choiceTech/choicedxself/ui/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.choiceTech.choicedxself.ui.settings
+
+import com.choiceTech.choicedxself.core.base.BaseFragment
+import com.choiceTech.choicedxself.databinding.FragmentSettingsBinding
+
+class SettingsFragment: BaseFragment<FragmentSettingsBinding>(
+    FragmentSettingsBinding::inflate
+) {
+
+}
